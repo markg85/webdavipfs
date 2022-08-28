fa90d0918f46205364c22dc9f3914823ba03a612
Mark Gaiser <markg85@gmail.com>
2022-08-28 17:17:11 +0200

    Initial code import.

---
+++ index.js
@@ -0,0 +1,18 @@
+import { v2 as webdav } from '@markg85/webdav-server';
+const IPFS_API = process.env.IPFS_API || 'http://127.0.0.1:5001';
+const WEBDAV_PORT = process.env.WEBDAV_PORT || 1900;
+import VFS from './lib/vfs.js';
+
+const server = new webdav.WebDAVServer({
+    port: WEBDAV_PORT
+});
+
+(async () => {
+    try {
+        server.setFileSystemSync("/", new webdav.PhysicalFileSystem(`/`, new VFS('', IPFS_API)));
+        server.start(() => console.log('READY'));
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
+})();
